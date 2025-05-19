e00b616f57a7f103de205d03f1b9e17f10e892db
JooHyun <pjh0629@gmail.com>
2025-05-19 17:25:18 -0400

    Gradle, navigation, topbar, bottombar setup

---
+++ app/src/main/java/com/example/nearbynote/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.example.nearbynote
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+//Application class
+@HiltAndroidApp
+class MyApplication : Application() {
+
+    /*    override fun onCreate() {
+            super.onCreate()
+
+            // use this code when you need to delete previous (room) database
+            deleteDatabase("healthfusion_database")
+        }*/
+}
