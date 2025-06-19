d83107398321645c249b2b96fad4790a4e979652
JooHyun <pjh0629@gmail.com>
2025-06-19 10:07:12 -0400

    Notification flow added. (clickable notification)

---
+++ app/src/main/java/com/example/nearbynote/nearbyNoteMainFunction/geoFenceAPI/util/CancelNotificationReceiver.kt
@@ -0,0 +1,17 @@
+package com.example.nearbynote.nearbyNoteMainFunction.geoFenceAPI.util
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class CancelNotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val notificationId = intent.getIntExtra("notificationId", -1)
+        if (notificationId != -1) {
+            val manager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.cancel(notificationId)
+        }
+    }
+}
