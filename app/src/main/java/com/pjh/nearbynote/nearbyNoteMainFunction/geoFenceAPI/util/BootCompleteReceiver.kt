f47f88d07472d8907166c201476b85db0e9cc24e
JooHyun <pjh0629@gmail.com>
2025-08-24 13:40:01 -0400

    Code tweaks

--- app/src/main/java/com/pjh/nearbynote/nearbyNoteMainFunction/geoFenceAPI/util/BootCompleteReceiver.kt
+++ app/src/main/java/com/pjh/nearbynote/nearbyNoteMainFunction/geoFenceAPI/util/BootCompleteReceiver.kt
@@ -54,7 +54,11 @@ class BootCompleteReceiver : BroadcastReceiver() {
                     if (ActivityCompat.checkSelfPermission(
                             appContext,
                             Manifest.permission.ACCESS_FINE_LOCATION
-                        ) == PackageManager.PERMISSION_GRANTED
+                        ) != PackageManager.PERMISSION_GRANTED ||
+                        ActivityCompat.checkSelfPermission(
+                            appContext,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
                     ) {
                         geofencingClient.addGeofences(request, getGeofencePendingIntent(appContext))
                     }
